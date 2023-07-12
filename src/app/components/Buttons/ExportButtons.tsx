c2923f5715b84ba0de4f7d2c69757dd707db2b73
bojan ratkovic <bojan.ratkovic@speck.agency>
2023-07-12 13:31:35 +0200

    styled added, arhitecture fix

--- src/app/components/Buttons/ExportButtons.tsx
+++ src/app/components/Buttons/ExportButtons.tsx
@@ -2,12 +2,26 @@ import { Button } from "antd";
 import React from "react";
 import { GreenButton } from "./styled";
 
-export default function ExportButtons() {
+export type exportButtonProps = {
+  xmlVisible: boolean,
+  csvVisible: boolean,
+  jsonVisible: boolean,
+};
+
+
+export default function ExportButtons(exportButtonProps: exportButtonProps) {
+  const { xmlVisible, csvVisible, jsonVisible } = exportButtonProps;
   return (
     <div>
-      <GreenButton disabled={false}>Preuzmi XML</GreenButton>
-      <Button>Preuzmi CSV</Button>
-      <Button danger type="primary" >Preuzmi JSON</Button>
+      {xmlVisible && <GreenButton disabled={false}>Preuzmi XML</GreenButton>}
+      {csvVisible && <Button>Preuzmi CSV</Button>}
+      {jsonVisible && <Button danger type="primary" >Preuzmi JSON</Button>}
     </div>
   );
+}
+
+ExportButtons.defaultProps = {
+  xmlVisible: true,
+  csvVisible: true,
+  jsonVisible: true,
 }
