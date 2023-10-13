db4484fd012f920dd35b333f0e6afe327f199a74
KristianValecic <79859420+KristianValecic@users.noreply.github.com>
2023-10-13 15:24:34 +0200

    test#9

--- src/app/components/TransparentnostSearch.tsx
+++ src/app/components/TransparentnostSearch.tsx
@@ -1,36 +1,99 @@
-import { Button, Input, Layout } from 'antd';
-import { Content } from 'antd/es/layout/layout';
-import { ColoredButton } from './buttons/styled';
-import { MainTitleH1 } from './styledComponents/MainTitleH1';
-import { FullWidthDiv } from './styledComponents/FullWidthDiv';
-import { TransparencyContent } from './search/styled';
+import { Input, Select } from 'antd';
+import { StyledSearchBar } from './table/styled.ts';
+import {
+  StyledTransparencyContent,
+  StyledTransparencyLayout,
+} from './search/styled.ts';
+import { StyledColoredButton } from './buttons/styled.ts';
+import { StyledMainTitleH1 } from './general/styled.ts';
 
 export default function TransparentnostSearch(props: any) {
+  const { Option } = Select;
   const { Search } = Input;
   const {
     onChangeInput,
     searchValueonSearchClick,
     searchValue,
+    currentYear,
+    onYearSelect,
     onSearchClick,
+    className,
+    buttonEnabled,
   } = props;
 
   return (
-    <TransparencyContent>
-      <MainTitleH1 $center>Transparentnost</MainTitleH1>
-      <Search
-        placeholder="Upišite ključnu riječ..."
-        enterButton={
-          <ColoredButton disabled={searchValue === ''} onClick={onSearchClick}>
-            Pretraži
-          </ColoredButton>
-        }
-        size="large"
-        onChange={onChangeInput}
-        value={searchValue}
-      />
-      <Button className="detailedSearchButton" type="text">
-        Detaljnija pretraga
-      </Button>
-    </TransparencyContent>
+    <StyledTransparencyLayout>
+      <StyledTransparencyContent>
+        <div className={className}>
+          <StyledMainTitleH1 $center>
+            <img src="../../../../img/grb.png" alt="" />
+            {/* <a href=""> */}Transparentnost{/* </a> */}
+          </StyledMainTitleH1>
+          {/* <Search
+            style={{ padding: '0' }}
+            placeholder="Upišite ključnu riječ..."
+            addonBefore={selectYear}
+            enterButton={
+              <StyledColoredButton
+                disabled={searchValue === ''}
+                onClick={onSearchClick}
+              >
+                Pretraži
+              </StyledColoredButton>
+            }
+            size="large"
+            onChange={onChangeInput}
+            value={searchValue}
+          /> */}
+          <StyledSearchBar>
+            <Input
+              placeholder="Upišite ključnu riječ..."
+              // onSearch={handleSearch}
+              onChange={onChangeInput}
+              className="search-input"
+            />
+            {/* this vanilla select is if the antd one doesnt style properly*/}
+            {/* <select>
+              <option value="2023">2023</option>
+              <option value="2023">2022</option>
+              <option value="2023">2021</option>
+              <option value="2023">2020</option>
+            </select> */}
+            <Select
+              rootClassName="dropdown"
+              defaultValue={currentYear}
+              className="dropdown"
+              onChange={onYearSelect}
+              options={[
+                {
+                  value: currentYear,
+                  label: currentYear,
+                },
+                {
+                  value: currentYear - 1,
+                  label: currentYear - 1,
+                },
+                {
+                  value: currentYear - 2,
+                  label: currentYear - 2,
+                },
+                {
+                  value: currentYear - 3,
+                  label: currentYear - 3,
+                },
+              ]}
+            ></Select>
+            <StyledColoredButton
+              disabled={!buttonEnabled}
+              onClick={onSearchClick}
+              type="primary"
+              className="search-button"
+            >
+              Pretraži
+            </StyledColoredButton>
+          </StyledSearchBar>
+        </div>
+      </StyledTransparencyContent>
+    </StyledTransparencyLayout>
   );
 }
