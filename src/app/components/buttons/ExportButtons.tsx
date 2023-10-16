9da08b42ff095b1d712632ee03beecb271356112
KristianValecic <79859420+KristianValecic@users.noreply.github.com>
2023-10-16 10:21:00 +0200

    change directory name to buttons

--- src/app/components/buttons/ExportButtons.tsx
+++ src/app/components/buttons/ExportButtons.tsx
@@ -31,7 +31,6 @@ export default function ExportButtons(exportButtonProps: exportButtonProps) {
   return (
     <>
       <p
-        className="throwaway"
         style={{
           textAlign: 'center',
           width: '100%',
